6b5935ad82d1275a338a1a3e82385a88d93f60b9
Xander Hill <xander.k.hill@gmail.com>
2025-05-23 18:42:49 -0500

    routes added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,6 @@
-import { Text, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Route, Routes, Navigate } from 'react-router-native';
+
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
 
@@ -13,7 +15,10 @@ const Main = () => {
     return (
         <View style={styles.container}>
             <AppBar/>
-            <RepositoryList/>
+            <Routes>
+                <Route path="/" element={<RepositoryList />} />
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
         </View>
     );
 };
