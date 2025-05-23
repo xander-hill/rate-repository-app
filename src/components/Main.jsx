8b33f675f6b69aa18ebd339fea0f0f846b55c7bc
Xander Hill <xander.k.hill@gmail.com>
2025-05-23 18:45:46 -0500

    signin component and route

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 import { StyleSheet, View } from 'react-native';
 import { Route, Routes, Navigate } from 'react-router-native';
 
+import SignIn from './SignIn';
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
 
@@ -17,6 +18,7 @@ const Main = () => {
             <AppBar/>
             <Routes>
                 <Route path="/" element={<RepositoryList />} />
+                <Route path="/signin" element={<SignIn />} />
                 <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
         </View>
