fbf8c1dd528b796aa1e8f34c7cb93234d03870a5
Xander Hill <xander.k.hill@gmail.com>
2025-05-27 15:28:13 -0500

    sign in form

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,43 @@
 import Text from './Text';
+import { TextInput, Pressable, View } from 'react-native';
+import { useFormik } from 'formik';
+
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+const SignInForm = ({ onSubmit }) => {
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+  });
+
+  return (
+    <View>
+      <TextInput
+        placeholder='username'
+        value={formik.values.username}
+        onChangeText={formik.handleChange('username')}
+      />
+      <TextInput
+        placeholder='password'
+        value={formik.values.password}
+        onChangeText={formik.handleChange('password')}
+        secureTextEntry
+      />
+      <Pressable onPress={formik.handleSubmit}>
+        <Text>Sign in</Text>
+      </Pressable>
+    </View>
+  );
+};
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+  return <SignInForm onSubmit={onSubmit} />;
 };
 
 export default SignIn;
