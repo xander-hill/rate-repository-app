46ab2781aa0db1653ec7238cd7619c2230a83b96
Xander Hill <xander.k.hill@gmail.com>
2025-05-28 15:18:29 -0500

    more authstorage context

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,7 +1,10 @@
 import { useMutation } from "@apollo/client";
 import { SIGN_IN } from "../graphql/mutations";
+import useAuthStorage from "./useAuthStorage";
 
 const useSignIn = () => {
+  const authStorage = useAuthStorage();
+
   const [mutate, result] = useMutation(SIGN_IN);
 
   const signIn = async ({ username, password }) => {
